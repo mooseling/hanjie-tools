0f470e5fbe357f9acb2b33f664cd121eac608557
James Stevenson <mooseling@hotmail.com>
2024-09-24 21:50:00 +0100

    Make check_overlaps algorithm account for dots

--- line_algorithms.py
+++ line_algorithms.py
@@ -12,38 +12,26 @@ type LineAlgorithm = Callable[[Line, LineClues], LineChanges]
 def check_overlaps(line: Line, block_lengths: LineClues) -> LineChanges:
     # For each block in a line, we will get its bounds. If there is overlap, we've found some squares
     # First step is to get min_end and max_start for each block
-    block_bound_list = [BlockBounds()] * len(block_lengths)
-    distance_travelled = 0
-
-    # TODO account for known blanks in the line
-
-    for block_index, block_length in enumerate(block_lengths):
-        block_bound_list[block_index].min_end = distance_travelled + block_length
-        distance_travelled += block_length + 1
-
-    distance_travelled = len(line)
-
-    for block_index, block_length in reversed(enumerate(block_lengths)):
-        block_bound_list[block_index].max_start = distance_travelled - block_length
-        distance_travelled -= block_length + 1
+    min_ends_of_blocks = get_min_ends_of_blocks(line, block_lengths)
+    max_starts_of_blocks = reversed(get_min_ends_of_blocks(reversed(line), reversed(block_lengths)))
 
     # Now we have the bounds for each block, so we check for overlaps
     line_changes: LineChanges = [Square.UNKNOWN] * len(line)
 
-    for block_index, block_bounds in enumerate(block_bound_list):
-        block_length = block_lengths[block_index]
-        found_length = block_bounds.min_end - block_bounds.max_start
+    for block_index, block_length in enumerate(block_lengths):
+        min_end = min_ends_of_blocks[block_index]
+        max_start = max_starts_of_blocks[block_index]
+        found_length = min_end - max_start
         
         if found_length > 0:
-            for square_index in range(block_bounds.max_start, block_bounds.min_end):
-                line_changes[square_index] = Square.FILLED
+            line_changes[max_start:min_end] = [Square.FILLED] * found_length
 
             # If we've found the entire block, it's easy to put the dots in now
             if found_length == block_length:
-                if block_bounds.max_start > 0:
-                    line_changes[block_bounds.max_start - 1] = Square.KNOWN_BLANK
-                if block_bounds.min_end < len(line) - 1:
-                    line_changes[block_bounds.min_end + 1] = Square.KNOWN_BLANK
+                if max_start > 0:
+                    line_changes[max_start - 1] = Square.KNOWN_BLANK
+                if min_end < len(line) - 1:
+                    line_changes[min_end + 1] = Square.KNOWN_BLANK
 
 
 def check_complete_blocks(line: Line, block_lengths: LineClues) -> LineChanges:
@@ -51,6 +39,26 @@ def check_complete_blocks(line: Line, block_lengths: LineClues) -> LineChanges:
     # Or narrow the possibilities
     pass
 
+
+def get_min_ends_of_blocks(line: Line, block_lengths: list[int]) -> list[int]:
+    block_bound_list = []
+    distance_travelled = 0
+
+    for block_length in block_lengths:
+        start_index = get_next_index_where_block_fits(line, block_length, distance_travelled)
+        block_bound_list.append(start_index + block_length)
+        distance_travelled = start_index + block_length + 1
+
+    return block_bound_list
+
+
+def get_next_index_where_block_fits(line: Line, block_length: int, start: int) -> int:
+    sub_line = line[start:block_length]
+    sub_index_of_wall = next((index for index, square in enumerate(sub_line) if square == Square.KNOWN_BLANK), False)
+    if sub_index_of_wall != False:
+        return get_next_index_where_block_fits(line, block_length, start + sub_index_of_wall + 1)
+    
+    return start
 
 
 
