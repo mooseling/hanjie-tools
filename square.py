def1347e9cffd748d4be7a5c9f32e154d24189ef
James Stevenson <mooseling@hotmail.com>
2024-09-26 13:11:57 +0100

    Change known-blank char to dot

--- square.py
+++ square.py
@@ -3,7 +3,7 @@ from enum import Enum
 class Square(Enum):
     UNKNOWN = ' ', 'unknown'
     FILLED = '#', 'filled'
-    KNOWN_BLANK = '/', 'blank'
+    KNOWN_BLANK = '⋅', 'blank'
 
     def get_grid_char(self):
         return self.value[0]
