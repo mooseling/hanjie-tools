958f1f300be92c5e3ee36b9feecc114ff0366bbe
James Stevenson <mooseling@hotmail.com>
2024-10-15 13:51:48 +0100

    Add index_of test and fix some other tests

--- test.py
+++ test.py
@@ -2,11 +2,19 @@ import unittest
 from block_utils import get_limits
 from data_classes import CluedBlock, Line
 from puzzle import Puzzle
-from utils import index_of_any
+from utils import index_of, index_of_any
 from square import Square
 
 
 class UtilsTest(unittest.TestCase):
+
+    def test_index_of(self) -> None:
+        line = [Square.UNKNOWN, Square.UNKNOWN, Square.UNKNOWN, Square.FILLED, Square.KNOWN_BLANK, Square.UNKNOWN]
+        self.assertEqual(index_of(line, Square.UNKNOWN), 0)
+        self.assertEqual(index_of(line, Square.FILLED), 3)
+        self.assertEqual(index_of(line, Square.KNOWN_BLANK), 4)
+        self.assertEqual(index_of(line, Square.UNKNOWN, 0), 0)
+        self.assertEqual(index_of(line, Square.UNKNOWN, 3), 5)
 
     def test_index_of_any(self) -> None:
         line = [Square.UNKNOWN, Square.UNKNOWN, Square.UNKNOWN, Square.FILLED, Square.KNOWN_BLANK, Square.UNKNOWN]
@@ -57,14 +65,14 @@ class BlockUtilsTest(unittest.TestCase):
         clued_block_1 = CluedBlock(3)
         clued_block_2 = CluedBlock(5)
         line_with_2_wiggle_room = Line([clued_block_1, clued_block_2], [Square.UNKNOWN] * 11)
-        self.assertEqual(get_limits(clued_block_1, line_with_2_wiggle_room), [0, 4])
-        self.assertEqual(get_limits(clued_block_2, line_with_2_wiggle_room), [4, 10])
+        self.assertEqual(get_limits(clued_block_1, line_with_2_wiggle_room), (0, 4))
+        self.assertEqual(get_limits(clued_block_2, line_with_2_wiggle_room), (4, 10))
 
         clued_block_3 = CluedBlock(3)
         clued_block_4 = CluedBlock(3)
         line_with_1_wiggle_room = Line([clued_block_3, clued_block_4], [Square.UNKNOWN] * 8)
-        self.assertEqual(get_limits(clued_block_3, line_with_1_wiggle_room), [0, 3])
-        self.assertEqual(get_limits(clued_block_4, line_with_1_wiggle_room), [4, 7])
+        self.assertEqual(get_limits(clued_block_3, line_with_1_wiggle_room), (0, 3))
+        self.assertEqual(get_limits(clued_block_4, line_with_1_wiggle_room), (4, 7))
 
 
 if __name__ == '__main__':
