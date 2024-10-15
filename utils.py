78416663a90d283d63118e53564905c059ca0362
James Stevenson <mooseling@hotmail.com>
2024-10-15 14:05:38 +0100

    Improve speed of index_of and index_of_any
    Improved from
    1.210 (index_of) and 0.809 (index_of_any) to
    1.035 (index_of) and 0.621 (index_of_any) (cumulative)
    By replacing for-in-enumerate with for-in-range

--- utils.py
+++ utils.py
@@ -3,17 +3,16 @@ def rev_list[T](list_to_reverse:list[T]) -> list[T]:
 
 
 def index_of[T](container: list[T], value: T, start_index: int = 0) -> int:
-    for index, item in enumerate(container[start_index:]):
-        if item == value:
-            return index + start_index
+    for index in range(start_index, len(container)):
+        if container[index] == value:
+            return index
     return -1
 
 
 def index_of_any[T](container: list[T], values: list[T], start_index: int = 0) -> int:
-
-    for index, item in enumerate(container[start_index:]):
+    for index in range(start_index, len(container)):
         for target_value in values:
-            if item == target_value:
-                return index + start_index
+            if container[index] == target_value:
+                return index
 
     return -1
