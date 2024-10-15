0d23c7ba7bfda552be77e8b9c4b08b15c3583d78
James Stevenson <mooseling@hotmail.com>
2024-10-15 13:53:28 +0100

    Improve speed of index_of and index_of_any
    Running main, we get cumulative times:
    2.049s -> 1.210s (index_of)
    and 2.773s -> 0.809s (index_of_any)

--- utils.py
+++ utils.py
@@ -3,13 +3,17 @@ def rev_list[T](list_to_reverse:list[T]) -> list[T]:
 
 
 def index_of[T](container: list[T], value: T, start_index: int = 0) -> int:
-    return next((index + start_index for index, square in enumerate(container[start_index:]) if square == value), -1)
+    for index, item in enumerate(container[start_index:]):
+        if item == value:
+            return index + start_index
+    return -1
 
 
 def index_of_any[T](container: list[T], values: list[T], start_index: int = 0) -> int:
-    return next(
-        (index + start_index for index, square in enumerate(container[start_index:]) if next(
-            (index2 for index2, value in enumerate(values) if value == square), -1
-            ) > -1),
-        -1
-    )
+
+    for index, item in enumerate(container[start_index:]):
+        for target_value in values:
+            if item == target_value:
+                return index + start_index
+
+    return -1
