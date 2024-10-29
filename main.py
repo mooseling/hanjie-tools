0f29d6b77885b0086d9641fd45c13d36d001a7e5
James Stevenson <mooseling@hotmail.com>
2024-10-29 09:32:43 +0000

    Switch to pygame visualiser

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import time
 from example_hanjies import poolside as puzzle
-from line_algorithms import LineAlgorithm, LineChanges, check_possible_visible_clued_mappings, check_overlaps, check_edge_hints
+from line_algorithms import LineAlgorithm, LineChanges, check_possible_visible_clued_mappings, check_overlaps, check_edge_hints, find_known_blank_regions
 from square import Square
-from visualise import visualise_puzzle
+from visualise_pygame import PygamePuzzleVisualiser
+import multiprocessing as mp
+
+
+# I'm using a second process to run the pygame visualiser. Getting that to work has required this:
+if __name__ == '__main__':
+    print("Setting process start method")
+    mp.set_start_method('fork')
 
 
 def has_changes(line_changes: LineChanges, line_squares: list[Square]) -> bool:
@@ -13,16 +19,15 @@ def has_changes(line_changes: LineChanges, line_squares: list[Square]) -> bool:
     return False
 
 
+visualiser = PygamePuzzleVisualiser(puzzle)
 
-frame_sleep_time = 0.1 # 0.25
-
-print(visualise_puzzle(puzzle))
-
-line_algorithm_list: list[LineAlgorithm] = [check_overlaps, check_edge_hints, check_possible_visible_clued_mappings]
+line_algorithm_list: list[LineAlgorithm] = [check_overlaps, check_edge_hints, check_possible_visible_clued_mappings, find_known_blank_regions]
 
 anything_has_changed_this_round = True
 while anything_has_changed_this_round:
     anything_has_changed_this_round = False
+
+    print("Top of puzzle solving loop...")
 
     for line_algorithm in line_algorithm_list:
         for row_index, row in enumerate(puzzle.get_rows()):
@@ -31,8 +36,7 @@ while anything_has_changed_this_round:
             anything_has_changed_this_round = result_causes_changes or anything_has_changed_this_round
             if result_causes_changes:
                 puzzle.apply_line_changes(algorithm_result, row_index=row_index)
-                print(visualise_puzzle(puzzle))
-                time.sleep(frame_sleep_time)
+                visualiser.visualise_puzzle(puzzle)
 
         for column_index, column in enumerate(puzzle.get_columns()):
             algorithm_result = line_algorithm(column)
@@ -40,5 +44,5 @@ while anything_has_changed_this_round:
             anything_has_changed_this_round = result_causes_changes or anything_has_changed_this_round
             if result_causes_changes:
                 puzzle.apply_line_changes(algorithm_result, column_index=column_index)
-                print(visualise_puzzle(puzzle))
-                time.sleep(frame_sleep_time)
+                visualiser.visualise_puzzle(puzzle)
+
